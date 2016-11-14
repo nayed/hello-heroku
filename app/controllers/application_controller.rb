b4562273df3d6fa269efa6ce5c625906517ba462
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-11-14 23:09:59 +0100

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hi people!"
+  end
+
+  def goodbye
+    render html: "Goodbye people!"
+  end
 end
